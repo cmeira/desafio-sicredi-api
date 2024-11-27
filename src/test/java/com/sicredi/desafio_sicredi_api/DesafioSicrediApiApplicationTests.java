6832466f991f0ade95faee33ff75af4d0084c50b
Carlos Meira <cmeiraa@gmail.com>
2024-11-27 12:42:41 -0300

    commit inicial

---
+++ src/test/java/com/sicredi/desafio_sicredi_api/DesafioSicrediApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sicredi.desafio_sicredi_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSicrediApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
